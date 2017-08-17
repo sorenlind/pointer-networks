9ac708fe26fff55b79a786fda8802fa404d1301f
shagun Sodhani <sshagunsodhani@gmail.com>
2017-08-17 11:19:48 +0530

    fixed imports, migrated to keras2.0 and formatted code

--- run.py
+++ run.py
@@ -1,22 +1,24 @@
-from keras.models import Model
-from keras.layers import LSTM, Input
-from keras.callbacks import LearningRateScheduler
-from keras.utils.np_utils import to_categorical
-from PointerLSTM import PointerLSTM
-import pickle
-import tsp
 import numpy as np
+from keras.callbacks import LearningRateScheduler
+from keras.layers import LSTM, Input
+from keras.models import Model
+from keras.utils.np_utils import to_categorical
+
+from PointerLSTM import PointerLSTM
+from tsp_data import Tsp
+
 
 def scheduler(epoch):
-    if epoch < nb_epochs/4:
+    if epoch < nb_epochs / 4:
         return learning_rate
-    elif epoch < nb_epochs/2:
-        return learning_rate*0.5
-    return learning_rate*0.1
+    elif epoch < nb_epochs / 2:
+        return learning_rate * 0.5
+    return learning_rate * 0.1
+
 
 print("preparing dataset...")
-t = tsp.Tsp()
-X, Y = t.next_batch(10000)
+t = Tsp()
+X, Y = t.next_batch(10)
 x_test, y_test = t.next_batch(1)
 
 YY = []
@@ -32,15 +34,17 @@ learning_rate = 0.1
 print("building model...")
 main_input = Input(shape=(seq_len, 2), name='main_input')
 
-encoder = LSTM(output_dim = hidden_size, return_sequences = True, name="encoder")(main_input)
-decoder = PointerLSTM(hidden_size, output_dim=hidden_size, name="decoder")(encoder)
+# encoder = LSTM(output_dim = hidden_size, return_sequences = True, name="encoder")(main_input)
+encoder = LSTM(units=hidden_size, return_sequences=True, name="encoder")(main_input)
+# decoder = PointerLSTM(hidden_size, output_dim=hidden_size, name="decoder")(encoder)
+decoder = PointerLSTM(hidden_shape=hidden_size, units=hidden_size, name="decoder")(encoder)
 
-model = Model(input=main_input, output=decoder)
+model = Model(inputs=main_input, outputs=decoder)
 model.compile(optimizer='adadelta',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(X, YY, nb_epoch=nb_epochs, batch_size=64,callbacks=[LearningRateScheduler(scheduler),])
+model.fit(X, YY, epochs=nb_epochs, batch_size=64, callbacks=[LearningRateScheduler(scheduler), ])
 print(model.predict(x_test))
 print("------")
 print(to_categorical(y_test))
